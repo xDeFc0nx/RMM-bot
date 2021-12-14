5d568d3c656c900c15ecfd2cbe58dd838424352b
xDeFc0nx <nehartale8@gmail.com>
2021-12-14 01:37:40 +0100

    re making the bot to discord.js 13

---
+++ src/Commands/verify.js
@@ -0,0 +1,32 @@
+const Command = require("../Structures/Command.js");
+const Discord = require("discord.js");
+module.exports = new Command({
+  name: "verify",
+  description: "Shows the ping of the bot!",
+  async run(message, args, client) {
+    let member = message.mentions.members.first();
+    let nonVerified = message.guild.roles.cache.find(
+      (r) => r.id === "881530659028496404"
+    );
+    let verified = message.guild.roles.cache.find(
+      (r) => r.id === "881540349649113099"
+    );
+
+    const user = message.mentions.users.first();
+    if (!user) {
+      message.reply("No User specified");
+    }
+    if (user) {
+      member.roles.remove(nonVerified);
+      member.roles.add(verified);
+      message.channel.send("Verified " + user.toString());
+
+      client.channels.cache
+        .get("880565815487918227")
+        .send(
+          user.toString() +
+            " Asalmualikum Warahmatullahi Wabarakatuh, Welcome to RMM(Real Muslim Men) Please read rules in <#880565815487918221>, you can read more information about roles in <#880565815487918222>,get some roles in <#880565815487918223>, and introduce yourself in <#890121772714823680> , Thanks for joining us! :slight_smile:   "
+        );
+    }
+  },
+});
