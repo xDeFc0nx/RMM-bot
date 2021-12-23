dfb5d3490823a5de2f3a65c7ca9cece27cf0324b
xDeFc0nx <nehartale8@gmail.com>
2021-12-23 11:58:22 +0100

    bot updated to 1.0.0 re made the entire bot

--- src/Commands/ping.js
+++
@@ -1,9 +0,0 @@
-const Command = require("../Structures/Command.js");
-
-module.exports = new Command({
-  name: "ping",
-  description: "Shows the ping of the bot!",
-  async run(message, args, client) {
-    message.reply(`ping: ${client.ws.ping} ms.`);
-  },
-});
