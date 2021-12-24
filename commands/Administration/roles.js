2280e775522581f654431af3e640906df7d87192
xDeFc0nx <nehartale8@gmail.com>
2021-12-24 02:01:22 +0100

    some more commands

--- commands/Administration/roles.js
+++ commands/Administration/roles.js
@@ -14,7 +14,7 @@ module.exports = {
         .setCustomId("test")
         .setMaxValues(5)
         .setMinValues(1)
-        .setPlaceholder("test test")
+        .setPlaceholder("please chouse 1 or more roles from below")
         .addOptions([
           {
             label: "announcement",
@@ -67,74 +67,70 @@ module.exports = {
         ])
     );
     const embed = new MessageEmbed()
-      .setColor("BLUE")
-      .setDescription("use the button below to get roles");
+    .setColor("BLUE")
+    .setDescription("use the button below to get roles");
     const m = await message.channel.send({
       embeds: [embed],
       components: [row],
     });
-
+    
     const collector = m.createMessageComponentCollector({
-      filter: (i) => i.user.id === message.author.id,
+      filter: i=>i.user.id === message.author.id,
       time: 60000,
     });
-
-    collector.on("collect", async (i) => {
+    
+    collector.on("collect", async i => {
+      interaction.deferUpdate()
       const announcement = i.guild.roles.cache.get("889163405842350140");
       const dead = i.guild.roles.cache.get("889159972938711070");
       const partnership = i.guild.roles.cache.get("889161805891510293");
-      const roleadd = new Array()
-      const roleremoved = new Array()
+      const roleadded = new Array();
+      const roleremoved = new Array();
 
-      if (i.values[0] === "announcement" || i.value[1] === "announcement") {
+      if (i.values[0] === `announcement` || i.values[1] === `announcement`) {
         if (i.member.roles.cache.has(announcement.id)) {
-          roleremoved.push(announcement)
-        i.member.roles.remove("889163405842350140");
+          roleremoved.push(announcement);
+          i.member.roles.remove("889163405842350140");
           i.reply({
             content: `Removed the ${announcement} role`,
             ephemeral: true,
           });
         } else {
-          roleadd.push(announcement)
+          roleadded.push(announcement);
           i.member.roles.add("889163405842350140");
-        
         }
-      
       }
-      if (i.values[0] === "partnership" || i.value[1] === "partnership") {
+      if (i.values[0] === `partnership` || i.values[1] === `partnership`) {
         if (i.member.roles.cache.has(dead.id)) {
           i.member.roles.remove("889161805891510293");
-          roleremoved.push(partnership)
+          roleremoved.push(partnership);
           i.reply({
             content: `Removed the ${partnership} role`,
             ephemeral: true,
           });
         } else {
-          roleadd.push(partnership)
+          roleadded.push(partnership);
           i.member.roles.add("889161805891510293");
-        
         }
       }
       if (
-        i.values[0] === "ping for dead server" ||
-        i.value[1] === "ping for dead server"
+        i.values[0] === `ping for dead server` ||
+        i.values[1] === `ping for dead server`
       ) {
         if (i.member.roles.cache.has(dead.id)) {
-          roleremoved.push(dead)
+          roleremoved.push(dead);
           i.member.roles.remove("889159972938711070");
 
           i.reply({ content: `Removed the ${dead} role`, ephemeral: true });
         } else {
-          roleadd.push(dead)
+          roleadded.push(dead);
           i.member.roles.add("889159972938711070");
         }
         const rembed = new MessageEmbed()
-        .setColor("BLUE")
-        .setDescription(`Added the role(s): ${roleadd.join(",")}/nRemoved the Role(s): ${roleremoved.join(",")} `)
-        i.reply({embeds:[rembed], ephemeral: true})
+          .setColor("BLUE")
+          .setDescription(`Added the role(s): ${roleadded.join(", ")}\nRemoved the Role(s): ${roleremoved.join(", ")} `);
+          i.reply({ embeds: [rembed], ephemeral: true });
       }
-   
     });
-    },
-  };
- 
+  },
+};
--- index.js
+++ index.js
@@ -3,39 +3,39 @@ const config = require(`./botconfig/config.json`);
 const settings = require(`./botconfig/settings.json`);
 const colors = require("colors");
 const client = new Discord.Client({
-    //fetchAllMembers: false,
-    //restTimeOffset: 0,
-    //restWsBridgetimeout: 100,
-    shards: "auto",
-    allowedMentions: {
-      parse: [ ],
-      repliedUser: false,
+  //fetchAllMembers: false,
+  //restTimeOffset: 0,
+  //restWsBridgetimeout: 100,
+  shards: "auto",
+  allowedMentions: {
+    parse: [],
+    repliedUser: false,
+  },
+  partials: ["MESSAGE", "CHANNEL", "REACTION"],
+  intents: [
+    Discord.Intents.FLAGS.GUILDS,
+    Discord.Intents.FLAGS.GUILD_MEMBERS,
+    Discord.Intents.FLAGS.GUILD_BANS,
+    Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+    Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
+    Discord.Intents.FLAGS.GUILD_WEBHOOKS,
+    Discord.Intents.FLAGS.GUILD_INVITES,
+    Discord.Intents.FLAGS.GUILD_VOICE_STATES,
+    Discord.Intents.FLAGS.GUILD_PRESENCES,
+    Discord.Intents.FLAGS.GUILD_MESSAGES,
+    Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING,
+    Discord.Intents.FLAGS.DIRECT_MESSAGES,
+    Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+    Discord.Intents.FLAGS.DIRECT_MESSAGE_TYPING,
+  ],
+  presence: {
+    activity: {
+      name: `Music`,
+      type: "LISTENING",
     },
-    partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
-    intents: [ 
-        Discord.Intents.FLAGS.GUILDS,
-        Discord.Intents.FLAGS.GUILD_MEMBERS,
-        //Discord.Intents.FLAGS.GUILD_BANS,
-        //Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
-        //Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
-        //Discord.Intents.FLAGS.GUILD_WEBHOOKS,
-        //Discord.Intents.FLAGS.GUILD_INVITES,
-        Discord.Intents.FLAGS.GUILD_VOICE_STATES,
-        //Discord.Intents.FLAGS.GUILD_PRESENCES,
-        Discord.Intents.FLAGS.GUILD_MESSAGES,
-        Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-        //Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING,
-        //Discord.Intents.FLAGS.DIRECT_MESSAGES,
-        //Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
-        //Discord.Intents.FLAGS.DIRECT_MESSAGE_TYPING
-    ],
-    presence: {
-      activity: {
-        name: `Music`, 
-        type: "LISTENING", 
-      },
-      status: "online"
-    }
+    status: "online",
+  },
 });
 //Define some Global Collections
 client.commands = new Discord.Collection();
@@ -45,19 +45,9 @@ client.aliases = new Discord.Collection();
 client.categories = require("fs").readdirSync(`./commands`);
 //Require the Handlers                  Add the antiCrash file too, if its enabled
 ["events", "commands", "slashCommands", settings.antiCrash ? "antiCrash" : null]
-    .filter(Boolean)
-    .forEach(h => {
-        require(`./handlers/${h}`)(client);
-    })
+  .filter(Boolean)
+  .forEach((h) => {
+    require(`./handlers/${h}`)(client);
+  });
 //Start the Bot
-client.login(config.token)
-
-/**
- * @INFO
- * Bot Coded by Tomato#6966 | https://discord.gg/milrato
- * @INFO
- * Work for Milrato Development | https://milrato.eu
- * @INFO
- * Please mention Him / Milrato Development, when using this Code!
- * @INFO
- */
+client.login(config.token);
