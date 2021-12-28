9a3a4f0a1596ab1120bfc08c4616e2350e7e4e1c
UnevenCoder <crafterandcoder@gmail.com>
2021-12-28 20:22:39 +0300

    unban done

--- commands/Moderation/unban.js
+++ commands/Moderation/unban.js
@@ -21,7 +21,7 @@ module.exports = {
   argsmissing_message: "You are missing the text you wanna add to the message!", //Message if the user has not enough args / not enough plus args, which will be sent, leave emtpy / dont add, if you wanna use command.usage or the default message! [OPTIONAL]
   argstoomany_message: "You are having too many arguments for this Command!", //Message if the user has too many / not enough args / too many plus args, which will be sent, leave emtpy / dont add, if you wanna use command.usage or the default message! [OPTIONAL]
   run: async (client, message, args, plusArgs, cmdUser, text, prefix) => {
-    if (!message.member.hasPermission("BAN_MEMBERS")) {
+    if (!message.member.permissions.has("BAN_MEMBERS")) {
       message.channel.send(
         "You aren't based enough to use this command [Reality "
       );
